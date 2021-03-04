c5e142b403e2097c425c920bc74f203c07c15eeb
Kaan Han Günay <kaangunay16@gmail.com>
2021-03-04 09:52:00 +0300

    Defining urls in app and route it

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('home')
+
+
+def product(request):
+    return HttpResponse('products')
+
+
+def customer(request):
+    return HttpResponse('customer')
